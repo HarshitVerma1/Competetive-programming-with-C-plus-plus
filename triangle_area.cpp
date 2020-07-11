124d11e60626cf379d452317ca9bc8d37df404a9
Harshit Verma <51871652+HarshitVerma1@users.noreply.github.com>
2020-07-11 21:26:55 +0530

    git add .

---
+++ triangle_area.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float b , h , c;
+    cout<<"Enter The length of Base & Height of the perpendicular triangle respectively:";
+    cin>>b>>h;
+    c=0.5*b*h;
+    cout<<"Area of triangle will be : "<<c;
+}
