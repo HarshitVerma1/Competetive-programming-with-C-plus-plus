cccdd2f58fad4fda03fa00214424b458599072ad
HarshitVerma1 <harshitverma14366@gmail.com>
2020-09-09 10:20:16 +0530

    new file edited

--- triangle_area.cpp
+++ triangle_area.cpp
@@ -6,5 +6,5 @@ int main()
     cout<<"Enter The length of Base & Height of the perpendicular triangle respectively:";
     cin>>b>>h;
     c=0.5*b*h;
-    cout<<"Area of triangle will be : "<<c;
+    cout<<"Area of triangle will be : "<<c <<endl;
 }
