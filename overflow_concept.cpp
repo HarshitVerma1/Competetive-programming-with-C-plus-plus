818ee72e2f926cb6d95cc3ea7aa7c2bd70968acf
Harshit Verma <harshitverma14366@gmail.com>
2020-09-21 12:37:07 +0530

    new

---
+++ overflow_concept.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+    char a=128,b=127,c=-128,d=-129,e=INT8_MAX,f=INT8_MIN;
+    cout<<"MIN VALUE :"<<(int)a <<endl; // -128
+    cout<<"MAX VALUE :"<<(int)b <<endl; // +127
+    cout<<"MIN VALUE :"<<(int)c <<endl; // -128
+    cout<<"MAX VALUE :"<<(int)d <<endl; // +127
+    cout<<"INT8_MAX Value :"<<(int)e <<endl; // +127
+    cout<<"INT8_MIN Value :"<<(int)f <<endl; // -128
+
+}
