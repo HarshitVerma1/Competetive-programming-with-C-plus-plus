0174f62db1bae6ddef1fda406bd219899d706cfa
Harshit Verma <harshitverma14366@gmail.com>
2020-09-25 12:29:02 +0530

    new

---
+++ C++/11.pattern1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 0; i <= 5; i++)
+    {
+        for (int J = 0; J < i; J++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
---
+++ C++/15.pattern5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (char i = 'E'; i >= 'A'; i--)
+    {
+        for (char j = 'E'; j >= 'A'; j--)
+        {
+            printf("%c ", i);
+        }
+        printf("\n");
+    }
+}
