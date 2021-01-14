47bf612413296320f65a3535049840a66282ecff
Harshit verma <terminator@pop-os.localdomain>
2021-01-14 09:41:18 +0530

    new

--- practice_c++/15.Pointer_to_Object.cpp
+++ practice_c++/15.Pointer_to_Object.cpp
@@ -23,3 +23,47 @@ int main()
     cout << r1.area() << endl;
     cout << r1.parameters() << endl;
 }
+
+// #include <iostream>
+// using namespace std;
+// class Rectangle
+// {
+// private:
+//     int length;
+//     int breadth;
+// public:
+//     void setLength(int a)
+//     {
+//         length = a;
+//     }
+//     void setBreadth(int b)
+//     {
+//         breadth = b;
+//     }
+//     int area()
+//     {
+//         return length * breadth;
+//     }
+//     int parameter()
+//     {
+//         return (2 * (length + breadth));
+//     }
+// };
+// int main()
+// {
+//     int m, n, q, r;
+//     cout << "Enter the Length and Breadth for 1st Rectangle respectively : " << endl;
+//     cin >> m >> n;
+//     cout << "Enter the Length and Breadth for 2nd Rectangle respectively : " << endl;
+//     cin >> q >> r;
+//     Rectangle *p = new Rectangle();
+//     Rectangle x;
+//     x.setLength(m);
+//     x.setBreadth(n);
+//     p->setLength(q);
+//     p->setBreadth(r);
+//     cout << "Area of x object : " << x.area() << endl;
+//     cout << "Parameter of x object : " << x.parameter() << endl;
+//     cout << "Area of p ptr object : " << p->area() << endl;
+//     cout << "Parameter of p ptr object : " << p->parameter() << endl;
+// }
--- practice_c++/check.cpp
+++ practice_c++/check.cpp
@@ -1,52 +1,44 @@
 #include <iostream>
 using namespace std;
-void printpath()
+class Rectangle
 {
-    cout << 1 << " " << 1 << "\n";
-    cout << 8 << " " << 8 << "\n";
-    cout << 7 << " " << 7 << "\n";
-    cout << 6 << " " << 8 << "\n";
-    cout << 1 << " " << 3 << "\n";
-    cout << 3 << " " << 1 << "\n";
-    cout << 8 << " " << 6 << "\n";
-    cout << 7 << " " << 5 << "\n";
-    cout << 8 << " " << 4 << "\n";
-    cout << 5 << " " << 1 << "\n";
-    cout << 1 << " " << 5 << "\n";
-    cout << 4 << " " << 8 << "\n";
-    cout << 3 << " " << 7 << "\n";
-    cout << 2 << " " << 8 << "\n";
-    cout << 1 << " " << 7 << "\n";
-    cout << 7 << " " << 1 << "\n";
-    cout << 8 << " " << 2 << "\n";
-}
+private:
+    int length;
+    int breadth;
+
+public:
+    void setLength(int a)
+    {
+        length = a;
+    }
+    void setBreadth(int b)
+    {
+        breadth = b;
+    }
+    int area()
+    {
+        return length * breadth;
+    }
+    int parameter()
+    {
+        return (2 * (length + breadth));
+    }
+};
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int r, c;
-        cin >> r >> c;
-        if (r == 1 && c == 1)
-        {
-            cout << 17 << "\n";
-            printpath();
-        }
-        else if (r == c)
-        {
-            cout << 18 << "\n";
-            cout << r << " " << c << "\n";
-            printpath();
-        }
-        else
-        {
-            cout << 19 << "\n";
-            cout << r << " " << c << "\n";
-            int d = (r + c) / 2;
-            cout << d << " " << d << "\n";
-            printpath();
-        }
-    }
-    return 0;
+    int m, n, q, r;
+    cout << "Enter the Length and Breadth for 1st Rectangle respectively : " << endl;
+    cin >> m >> n;
+    cout << "Enter the Length and Breadth for 2nd Rectangle respectively : " << endl;
+    cin >> q >> r;
+    Rectangle *p = new Rectangle();
+    Rectangle x;
+    x.setLength(m);
+    x.setBreadth(n);
+    p->setLength(q);
+    p->setBreadth(r);
+    cout << "Area of x object : " << x.area() << endl;
+    cout << "Parameter of x object : " << x.parameter() << endl;
+    cout << "Area of p ptr object : " << p->area() << endl;
+    cout << "Parameter of p ptr object : " << p->parameter() << endl;
 }
