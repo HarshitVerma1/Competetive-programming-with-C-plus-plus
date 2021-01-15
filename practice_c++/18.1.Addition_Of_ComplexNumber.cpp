29512573b20205576c9700fafd7b269c63acfc54
Harshit verma <terminator@pop-os.localdomain>
2021-01-15 11:15:47 +0530

    😍

---
+++ practice_c++/18.1.Addition_Of_ComplexNumber.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+class Complex
+{
+private:
+    int real, img;
+
+public:
+    Complex(int real = 0, int img = 0)
+    {
+        this->real = real;
+        this->img = img;
+    }
+
+    Complex add(Complex c2)
+    {
+        Complex temp;
+        temp.img = img + c2.img;
+        temp.real = real + c2.real;
+        display(temp);
+        return temp;
+    }
+    void display(Complex result)
+    {
+        cout << result.real << " + " << result.img << " i" << endl;
+    }
+};
+int main()
+{
+    int m, n, o, p;
+    cout << "Enter the img and real part of 1st Complex number repectively : " << endl;
+    cin >> m >> n;
+    cout << "Enter the img and real part of 2nd Complex number repectively : " << endl;
+    cin >> o >> p;
+    Complex c1(m, n), c2(o, p);
+    Complex result;
+    result = c1.add(c2);
+}
